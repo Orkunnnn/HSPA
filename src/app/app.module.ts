ff6c78c96d8330c8e07eb19c4f40bada93a69b12
Orkunnnn <orkunkonak1414@gmail.com>
2021-10-07 21:31:05 +0300

    Navbar, Property Card and Property List created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './components/property-card/property-card.component';
+import { PropertyListComponent } from './components/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PropertyCardComponent,
+    PropertyListComponent
   ],
   imports: [
     BrowserModule
